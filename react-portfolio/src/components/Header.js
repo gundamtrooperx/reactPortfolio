f78d544d47d94ed4fa855ed7d9c44a7b3eb9b049
Frank Carr <gundamtrooperx@gmail.com>
2020-10-25 00:32:32 -0700

    added position-relative to the navbar

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (<header className="header">
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark navbar-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark navbar-dark position-relative">
             <a className="navbar-brand" href="/">Frank Carr</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
